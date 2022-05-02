eabe818609989c124c7d19e85579bbfaf5747a81
Matheus3DD4 <matheus3dd4@gmail.com>
2022-05-02 09:24:26 -0300

    confirmar

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import CabecalhoHeader from "@/components/CabecalhoHeader.vue";
+import PrincipalContent from "@/components/PrincipalContent.vue";
+import RodapeFooter from "@/components/RodapeFooter.vue";
+
+export default {
+  components: { CabecalhoHeader, PrincipalContent, RodapeFooter },
+};
+</script>
+<template>
+  <CabecalhoHeader />
+  <PrincipalContent />
+  <RodapeFooter />
+</template>
+<style></style>
